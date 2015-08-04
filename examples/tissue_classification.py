8f185939cc2f8324029337c9b323f36420cdb75e
Alexis Roche <alexis.roche@gmail.com>
2015-08-04 17:20:22 +0200

    make example work

--- examples/tissue_classification.py
+++ examples/tissue_classification.py
@@ -6,12 +6,10 @@
 from __future__ import print_function # Python 2/3 compatibility
 
 import numpy as np
+import nibabel as nb
 
-from nipy import load_image, save_image
-from nipy.core.image.image_spaces import (make_xyz_image,
-                                          xyz_affine)
-from nipy.externals.argparse import ArgumentParser
-from nipy.algorithms.segmentation import BrainT1Segmentation
+from argparse import ArgumentParser
+from niseg import BrainT1Segmentation
 
 
 def fuzzy_dice(gold_ppm, ppm, mask):
@@ -61,14 +59,14 @@ def get_argument(dest, default):
         return val
 
 # Input image
-img = load_image(args.img[0])
+img = nb.load(args.img[0])
 
 # Input mask image
 mask_img = get_argument('mask', None)
 if mask_img == None:
     mask_img = img
 else:
-    mask_img = load_image(mask_img)
+    mask_img = nb.load(mask_img)
 
 # Other optional arguments
 niters = int(get_argument('niters', 25))
@@ -77,13 +75,12 @@ ngb_size = int(get_argument('ngb_size', 6))
 
 # Perform tissue classification
 mask = mask_img.get_data() > 0
-S = BrainT1Segmentation(img.get_data(), mask=mask, model='5k',
+S = BrainT1Segmentation(img, mask=mask, model='5k',
                         niters=niters, beta=beta, ngb_size=ngb_size)
 
 # Save label image
 outfile = 'hard_classif.nii'
-save_image(make_xyz_image(S.label, xyz_affine(img), 'scanner'),
-           outfile)
+nb.save(nb.Nifti1Image(S.label, img.get_affine()), outfile)
 print('Label image saved in: %s' % outfile)
 
 # Compute fuzzy Dice indices if a 3-class fuzzy model is provided
@@ -94,7 +91,7 @@ if not args.probc == None and \
     gold_ppm = np.zeros(S.ppm.shape)
     gold_ppm_img = (args.probc, args.probg, args.probw)
     for k in range(3):
-        img = load_image(gold_ppm_img[k])
+        img = nb.load(gold_ppm_img[k])
         gold_ppm[..., k] = img.get_data()
     d = fuzzy_dice(gold_ppm, S.ppm, np.where(mask_img.get_data() > 0))
     print('Fuzzy Dice indices: %s' % d)
